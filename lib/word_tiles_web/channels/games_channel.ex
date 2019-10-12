f40eda2545debd05abc3d9a1bff3add15f778a2e
Melisa Brown <melisa@Melisas-MacBook-Pro.local>
2019-10-12 18:13:40 -0400

    chat room

--- lib/word_tiles_web/channels/games_channel.ex
+++ lib/word_tiles_web/channels/games_channel.ex
@@ -1,7 +1,7 @@
 defmodule WordTilesWeb.GamesChannel do
   use WordTilesWeb, :channel
 
-  def join("games:lobby", payload, socket) do
+  def join("games:lobby" <> name, payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -20,6 +20,19 @@ defmodule WordTilesWeb.GamesChannel do
   def handle_in("shout", payload, socket) do
     broadcast socket, "shout", payload
     {:noreply, socket}
+  end
+
+  # HANDLE A NEW CHAT MESSAGE
+  def handle_in("new_chat_message", %{"body" => body}, socket) do
+    broadcast!(socket, "new_chat_message", %{
+      name: current_player(socket).name,
+      body: body
+    })
+    {:noreply, socket}
+  end
+
+  defp current_player(socket) do
+    socket.assigns.current_player
   end
 
   # Add authorization logic here as required.
