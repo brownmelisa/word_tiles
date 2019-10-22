c79731b0bae0b0a911ff9ca001869285ae15dc2d
Chuhan <zhangchuhan1995@gmail.com>
2019-10-22 10:27:59 -0400

    deploy 1st try

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4795
+
+echo "Starting app..."
+
+# Start to run in background from shell.
+#_build/prod/rel/memory/bin/memory start
+
+# Foreground for testing and for systemd change this.
+_build/prod/rel/memory/bin/memory start
+
+# TODO: Add a systemd service file
+#       to start your app on system boot.
+
