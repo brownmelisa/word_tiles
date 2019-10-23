844f60f864d0a5abb75e97e7c7c5b5de80042b9f
chuhan95 <zhang.chuh@husky.neu.edu>
2019-10-22 22:28:51 -0400

    deploy

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ export PORT=4795
 echo "Starting app..."
 
 # Start to run in background from shell.
-#_build/prod/rel/memory/bin/memory start
 
 # Foreground for testing and for systemd change this.
 
